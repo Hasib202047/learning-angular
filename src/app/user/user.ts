b5c02cb9dc54c55f4be8f08ec1172ee45b3ad917
Hasib202047 <hasibul.hoque129971@gmail.com>
2026-01-03 11:46:11 +0600

    day 2

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,8 +1,5 @@
-import {Component, computed, signal} from '@angular/core';
-import {DUMMY_USERS} from '../dummy-users';
+import {Component, computed, EventEmitter, input, Input, Output} from '@angular/core';
 
-
-let randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,11 +8,19 @@ let randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class User {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
-  updateIndex() {
-    console.log('clicked here')
-    randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+
+  // @Input({required: true}) name!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) id!: string;
+  @Output() selected = new EventEmitter();
+
+  // avatar = input.required<string>();
+  name = input.required<string>();
+
+  imagePath = computed(() => "/assets/users/" + this.avatar);
+
+  selectedUser(){
+    this.selected.emit(this.id);
   }
+
 }
